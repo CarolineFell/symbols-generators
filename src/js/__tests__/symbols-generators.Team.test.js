34bce132781e0bf98b59aad3d9626e23fa553e52
Gradt <carolinefell@yandex.ru>
2020-03-30 10:21:34 +0300

    Corrected the code

--- src/js/__tests__/symbols-generators.Team.test.js
+++ src/js/__tests__/symbols-generators.Team.test.js
@@ -1,48 +1,40 @@
+import Person from '../symbols-iterators.Person';
 import Team from '../symbols-generators.Team';
 
-const team = new Team();
-const iterator = team[Symbol.iterator]();
-
-test('Should iterate the Bowman', () => {
-  const character = iterator.next();
-  expect(character.value).toEqual({
-    name: 'Лучник',
-    type: 'Bowman',
-    health: 50,
-    level: 1,
-    attack: 40,
-    defence: 10,
-    damage: 10,
-  });
-});
-
-test('Should iterate the Shurikener', () => {
-  const character = iterator.next();
-  expect(character.value).toEqual({
-    name: 'Сюрикенщик',
-    type: 'Shurikener',
-    health: 100,
-    level: 1,
-    attack: 5,
-    defence: 50,
-    damage: 1,
-  });
-});
-
-test('Should iterate the Swordsman', () => {
-  const character = iterator.next();
-  expect(character.value).toEqual({
-    name: 'Мечник',
-    type: 'Swordsman',
-    health: 100,
-    level: 1,
-    attack: 10,
-    defence: 60,
-    damage: 50,
-  });
+test('Should add Team', () => {
+  const team = new Team();
+  const bowman = new Person(
+    {
+      name: 'Лучник',
+      type: 'Bowman',
+      health: 50,
+      level: 1,
+      attack: 40,
+      defense: 10,
+    },
+  );
+  team.add(bowman);
+  expect(team.hero[0]).toEqual(bowman);
 });
 
 test('Should iterate', () => {
-  const character = iterator.next();
-  expect(character.done).toEqual(true);
+  const heroArray = [];
+  heroArray.push(new Person({
+    name: 'Маг', type: 'Magician', health: 100, level: 2, attack: 20, defense: 15,
+  }));
+  heroArray.push(new Person({
+    name: 'Демон', type: 'Daemon', health: 'Infinity', level: 95, attack: 'Infinity', defense: 90,
+  }));
+  heroArray.push(new Person({
+    name: 'Зомби', type: 'Zombie', health: 70, level: 16, attack: 50, defense: 45,
+  }));
+  const team = new Team();
+  for (const hero of heroArray) {
+    team.add(hero);
+  }
+
+  let counter = 0;
+  for (const hero of team) {
+    expect(hero).toEqual(heroArray[counter++]);
+  }
 });
